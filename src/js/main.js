8f38fefb705a3d24e11ccec68a8972770c76bce0
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-05-06 15:22:50 +0200

    Senaste

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 "use strict";
 
+/* Anger sticky klass till navigationsmeny vid scroll */
 const headerEl = document.querySelector('header');
-
 window.addEventListener('scroll', () => {
     headerEl.classList.toggle('sticky', window.scrollY > 20);
 })
+
+/* Javascript för smooth scrolling vid klick av länk */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
