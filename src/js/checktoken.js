b4e9b63ea3e7333615a8e6349219859e46980569
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-05-11 13:08:16 +0200

    Lagt till säkerhet på adminpanel & spinneranimering

---
+++ src/js/checktoken.js
@@ -0,0 +1,33 @@
+
+/* Fil för att kontrollera om där finns en godkänd token */
+function checkAuthentication() {
+    const token = localStorage.getItem('token'); 
+    if (!token) {
+        window.location.href = '/index.html'
+    } else {
+        fetch('http://localhost:3000/api/admin-panel', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Ogiltig JWT-TOKEN');
+            }
+        })
+        .catch(error => {
+            console.error('Ogiltig JWT-TOKEN:', error);
+            window.location.href = '/index.html'
+        })
+    }
+}
+/* Logga ut funktion */
+const logOutEl = document.getElementById('logout-btn');
+logOutEl.addEventListener('click', logOut);
+function logOut() {
+    localStorage.removeItem('token');
+    window.location.href = '/index.html'
+}
+document.addEventListener('DOMContentLoaded', checkAuthentication);
--- src/js/login.js
+++ src/js/login.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const errormessageEl = document.getElementById('error-message');
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
+        showLoadingSpinner();
 
         fetch('http://localhost:3000/api/login', {
             method: 'POST',
@@ -17,7 +18,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }) 
         .then(response => {
             if (!response.ok) {
-                throw new Error('Inloggning misslyckades.')
+                if (response.status === 404) {
+                    hideLoadingSpinner()
+                    throw new Error('Ett fel inträffades.');
+                    
+                } else if (response.status === 401){
+                    hideLoadingSpinner()
+                    throw new Error('Fel användarnamn/lösenord.');
+                } else {
+                    hideLoadingSpinner()
+                    throw new Error('Något gick fel. Försök igen senare.');
+                }
             }
             return response.json();
         })
@@ -26,8 +37,19 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = '/admin.html'
         })
         .catch (error => {
+            hideLoadingSpinner()
             errormessageEl.textContent = 'Fel användarnamn / lösenord'
             console.error('Inloggning misslyckades', error);
         })
     }) 
 })
+
+/* Laddningsanimation som visar spinnern */
+function showLoadingSpinner() {
+    document.getElementById("loadingSpinner").style.display = "block";
+  }
+  /* Laddningsanimation som gömmer spinnern */
+  function hideLoadingSpinner() {
+    document.getElementById("loadingSpinner").style.display = "none";
+  }
+  
